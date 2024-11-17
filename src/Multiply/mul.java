a7bbbace76f25b3037af5083161d7876823f1f5a
keerthitejab <keerthitejabodapati85@gmail.com>
2024-11-17 19:27:50 +0530

    my own commit

---
+++ src/Multiply/mul.java
@@ -0,0 +1,17 @@
+package Multiply;
+
+public class mul {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=15;
+		
+		int c= a*b;
+		
+		System.out.println("multiplication of two numbers is: " +c);
+		
+
+	}
+
+}
